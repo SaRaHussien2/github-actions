8df63f055c6ae843c706d13a3c7a1d4414dafeab
SaRaHussien2 <57558677+SaRaHussien2@users.noreply.github.com>
2022-11-22 11:33:38 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+          ls 
+          java -version
+
+          echo Is Git installed
+          git --version
+          echo what about build tools?
+          mvn --version
+          gradle -version
+          echo what environment variables are available?
+          env
